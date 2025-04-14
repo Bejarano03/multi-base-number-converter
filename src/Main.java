931efac8e7031e4f19b82f03de39ccb2a3396e76
Marco Bejarano <marco.bejarano03@gmail.com>
2025-04-14 14:35:52 -0700

    inital commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * This program shows you an input location
+ * to enter a number, and then it converts it
+ * to the desired numerical base.
+ *
+ * @author Marco Bejarano Oseguera
+ * @version 04-14-2025
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Multi-Base Numerical Converter");
+        System.out.println();
+
+        System.out
+
+    }
+}
